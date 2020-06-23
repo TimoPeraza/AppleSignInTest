c61623a24ce3e6016c2096fb0cf5984f68ba5cd0
TimoPeraza <timothy@rootstrap.com>
2020-06-23 12:43:48 -0300

    add apple sign in

--- AppleSignIn/ViewController.swift
+++ AppleSignIn/ViewController.swift
@@ -7,14 +7,32 @@
 //
 
 import UIKit
+import AuthenticationServices
 
 class ViewController: UIViewController {
 
+    let appleProvider = AppleSignInClient()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let appleBtn = ASAuthorizationAppleIDButton()
+        self.view.addSubview(appleBtn)
+        appleBtn.addTarget(self, action: #selector(SignInWithAppleAction(sender:)), for: .touchUpInside)
+        appleBtn.center = self.view.center
     }
 
+    @objc
+    func SignInWithAppleAction(sender: ASAuthorizationAppleIDButton) {
+
+        appleProvider.handleAppleIdRequest(block: { fullName, email, token in
+            // receive data in login class.
+            
+            
+            
+        })
+    }
 
 }
 
